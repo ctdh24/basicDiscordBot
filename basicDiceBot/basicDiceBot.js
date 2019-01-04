93198d5c92a670ffa3f998692bb63005b6fc1e6f
calvin.huynh <calvin.huynh@objectbrains.com>
2019-01-04 10:08:31 -0800

    formatting

--- basicDiceBot/basicDiceBot.js
+++ basicDiceBot/basicDiceBot.js
@@ -1,23 +1,23 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-  client.on('ready', () => {
-      // list all servers
-      console.log("Servers:")
-      client.guilds.forEach((guild) => {
+client.on('ready', () => {
+    // list all servers
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
         console.log(" - " + guild.name)
         // List all channels
         guild.channels.forEach((channel) => {
-          console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
 
-          // say hello to specific channel on start
-          if(channel.name === "general" && channel.type === "text"){
-            var generalChannel = client.channels.get(channel.id)
-            generalChannel.send("Hello, world!")
-          }
+            // say hello to specific channel on start
+            if(channel.name === "general" && channel.type === "text"){
+                var generalChannel = client.channels.get(channel.id)
+                generalChannel.send("Hello, world!")
+            }
         })
-      })
-  })
+    })
+})
 
   client.on('message', (receivedMessage) => {
       if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
@@ -29,7 +29,7 @@ const client = new Discord.Client()
       }
   })
 
-  function processCommand(receivedMessage){
+function processCommand(receivedMessage){
     let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
     let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
     let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
@@ -38,24 +38,22 @@ const client = new Discord.Client()
     console.log("Command received: " + primaryCommand)
     console.log("Arguments: " + arguments) // There may not be any arguments
 
-    if (primaryCommand == "help") {
+    if (primaryCommand == "?") {
         helpCommand(arguments, receivedMessage)
     } else if (primaryCommand == "multiply") {
         multiplyCommand(arguments, receivedMessage)
     } else if (primaryCommand == "roll") {
         rollCommand(arguments, receivedMessage)
     } else {
-        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
-    }
-  }
-  function helpCommand(arguments, receivedMessage) {
-    if (arguments.length > 0) {
-        receivedMessage.channel.send("It looks like you might need help with " + arguments)
-    } else {
-        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+        receivedMessage.channel.send("I don't understand the command. Type `!?` for a list of commands.")
     }
 }
 
+function helpCommand(arguments, receivedMessage) {
+    receivedMessage.channel.send("`!roll [dice notation]`")
+}
+
+//test command
 function multiplyCommand(arguments, receivedMessage) {
     if (arguments.length < 2) {
         receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
@@ -68,37 +66,36 @@ function multiplyCommand(arguments, receivedMessage) {
     receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
 }
 
+//!roll
 function rollCommand(arguments, receivedMessage) {
-  if (arguments === ""){
-    receivedMessage.channel.send("Not enough dice to roll. Try `!roll 1d6` to roll 1 six-sided dice")
-    return
-  }
-  else{
-    var sum = 0;
-    var resultString = "";
-    arguments.forEach((value)=>{
-      var regex = /^(\d*)d(\d+)([-+]\d+)*x*(\d+)*/;
-      values = value.match(regex).slice(1),
-      a = parseInt(values[0]) || 1,
-      b = parseInt(values[1]),
-      c = parseInt(values[2]) || 0,
-      d = parseInt(values[3]) || 1;
-      //console.log(d)
-      for (j = 0; j < d; j++){
-        for (i = 0; i < a; i++){
-          var randomNumber = Math.floor(Math.random() * (b)) + 1;
-          resultString += randomNumber + " + ";
-          sum += randomNumber;
-        }
-      }
-      sum+= c*d;
-      if (d === 1)
-        resultString += c + " ";
-      else
-        resultString += "("+c + "*" + d + ") ";
-    })
-    receivedMessage.channel.send(receivedMessage.author.toString() + " has rolled: " + resultString + "= " + sum);
-  }
+    if (arguments === ""){
+        receivedMessage.channel.send("Not enough dice to roll. Try `!roll 1d6` to roll 1 six-sided dice")
+        return
+    }
+    else{
+        var sum = 0;
+        var resultString = "";
+        arguments.forEach((value)=>{
+            var regex = /^(\d*)d(\d+)([-+]\d+)*x*(\d+)*/;
+            values = value.match(regex).slice(1),
+            a = parseInt(values[0]) || 1,
+            b = parseInt(values[1]),
+            c = parseInt(values[2]) || 0,
+            d = parseInt(values[3]) || 1;
+            //console.log(d)
+            for (j = 0; j < d; j++){
+                for (i = 0; i < a; i++){
+                    var randomNumber = Math.floor(Math.random() * (b)) + 1;
+                    resultString += randomNumber + " + ";
+                    sum += randomNumber;
+                }
+            }
+            sum+= c*d;
+            if (d === 1) resultString += c + " ";
+            else resultString += "("+c + "*" + d + ") ";
+        })
+        receivedMessage.channel.send(receivedMessage.author.toString() + " has rolled: " + resultString + "= " + sum);
+    }
 }
 
 // Get your bot's secret token from:
